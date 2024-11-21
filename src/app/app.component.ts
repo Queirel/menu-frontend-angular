a8f79f69cdf77b82105b80a4272a86dbbe35a695
Fede <federicoqueirel@hotmail.com>
2024-11-21 06:18:28 -0300

    form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-
-  value = 1;
-
-    increment = (): void =>{
-      this.value++
-    }
-
-    decrement = (): void =>{
-      this.value--
-    }
 
 
 }
---
+++ src/app/form/form.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Usuario } from './usuario';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css',
+})
+export class FormComponent {
+  constructor(private formBuilder: FormBuilder) {}
+
+  usuario = new Usuario();
+  apellidos = ['lope', 'otro', 'coso'];
+
+  usuarioForm = this.formBuilder.group({
+    nombre: ['', Validators.required],
+    apellido: ['', Validators.required],
+  });
+}
