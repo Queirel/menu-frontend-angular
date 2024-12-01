7a152c59895c07cf55c0d7167f8a987787a56c50
Fede <federicoqueirel@hotmail.com>
2024-12-01 06:07:37 -0300

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MainViewComponent } from './main-view/main-view.component';
+import { EgresosComponent } from './egresos/egresos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MainViewComponent],
+  imports: [EgresosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/egresos/egresos.component.ts
+++ src/app/egresos/egresos.component.ts
@@ -1,12 +1,14 @@
+import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
+import { PipegresoPipe } from './pipegreso.pipe';
 
 @Component({
   selector: 'app-egresos',
   standalone: true,
-  imports: [],
+  imports: [UpperCasePipe, PipegresoPipe],
   templateUrl: './egresos.component.html',
   styleUrl: './egresos.component.css'
 })
 export class EgresosComponent {
-
+mensaje: string = 'mensaje'
 }
